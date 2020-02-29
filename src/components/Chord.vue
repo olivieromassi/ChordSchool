8e3e78623af9f17d5b46e7a6b0e26993a62ac3d1
olivieromassi <oliviero.massi@mail.polimi.it>
2020-02-29 18:43:11 +0100

    added method to Chord component

--- src/components/Chord.vue
+++ src/components/Chord.vue
@@ -20,6 +20,24 @@
                 chordQuality: ''
             }
         },
+        methods: {
+            buildChord(refScale, grade) {
+                this.tonic = refScale[(grade - 1) % 7];
+                this.third = refScale[(grade + 1) % 7];
+                this.fifth = refScale[(grade + 3) % 7];
+                this.seventh = refScale[(grade + 5) % 7];
+                switch (grade) {
+                   case 1 : this.chordQuality = 'MA7'; break;
+                   case 2 : this.chordQuality = 'mi7'; break;
+                   case 3 : this.chordQuality = 'mi7'; break;
+                   case 4 : this.chordQuality = 'MA7'; break;
+                   case 5 : this.chordQuality = 'MA7'; break;
+                   case 6 : this.chordQuality = 'mi7'; break;
+                   case 7 : this.chordQuality = 'dim7'; break;
+                   default: this.chordQuality = ''; break;
+                }
+            }
+        },
         computed: {
             chordName() {
                 return this.tonic + this.chordQuality;
