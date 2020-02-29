6de221a3003ee934fd6423d19b7fd79bd3a63853
olivieromassi <oliviero.massi@mail.polimi.it>
2020-02-29 16:37:38 +0100

    First commit and ideas

---
+++ src/components/Chord.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>{{ chordName }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Chord",
+        data: function () {
+            return {
+             chordName: "CMA7",
+             tonic: "C3",
+             third: "E3",
+             fifth: "G3",
+             seventh: "B3"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Modes.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul>
+        <li v-for="note in scales" :key="note">{{ note }}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Modes",
+        data: function () {
+            return {
+                scales: [['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+                    ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#'],
+                    ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
+                    ['Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D'],
+                    ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#'],
+                    ['F', 'G', 'A', 'Bb', 'C', 'D', 'E'],
+                    ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'E#'],
+                    ['G', 'A', 'B', 'C', 'D', 'E', 'F#'],
+                    ['Ab', 'Bb', 'C', 'Db', 'Eb', 'F', 'G'],
+                    ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
+                    ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'],
+                    ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#']]
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
