6bb61b474cc849f75a253c4a2a8157e1255aba3b
olivieromassi <oliviero.massi@mail.polimi.it>
2020-02-29 17:53:07 +0100

    Added component

---
+++ src/components/Modes.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+        <li v-for="chord in modalChords" :key="chord"><Chord></Chord></li>
+    </ul>
+</template>
+
+<script>
+    import Chord from "@/components/Chord";
+    export default {
+        name: "Modes",
+        components: {Chord},
+        data: function () {
+            return{
+                modalChords : []
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
