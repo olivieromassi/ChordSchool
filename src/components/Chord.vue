849f614af9429cfc07bbb4a03d8a9bdac6ab1a77
olivieromassi <oliviero.massi@mail.polimi.it>
2020-02-29 17:53:24 +0100

    modified component

--- src/components/Chord.vue
+++ src/components/Chord.vue
@@ -7,6 +7,10 @@
 <script>
     export default {
         name: "Chord",
+        props: {
+          selectedScale: {
+          }
+        },
         data: function () {
             return {
                 tonic: '',
