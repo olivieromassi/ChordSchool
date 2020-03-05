a651ed83c03daac5675fdaf78168b78e9593e16d
olivieromassi <oliviero.massi@mail.polimi.it>
2020-03-05 19:13:05 +0100

    Added imports needed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import { store } from './store'
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  render: h => h(App)
+}).$mount('#app');
