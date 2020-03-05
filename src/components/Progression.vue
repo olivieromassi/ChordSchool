806b2f7210de2d385efd5e3018153a17eed8e7a0
olivieromassi <oliviero.massi@mail.polimi.it>
2020-03-05 18:09:40 +0100

    Added Progression component

---
+++ src/components/Progression.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>{{features}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Progression",
+        props: {
+            features: {
+                tonic: '',
+                third: '',
+                fifth: '',
+                seventh: '',
+                chordQuality: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
