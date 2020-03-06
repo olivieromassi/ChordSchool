e03a80d39d8c6b1984f459ad28b95d31f0e7094a
olivieromassi <oliviero.massi@mail.polimi.it>
2020-03-06 17:36:08 +0100

    Added component

---
+++ src/components/KeyReference.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <label> SELECT A KEY REFERENCE
+            <select v-model="selectedKey" @change="setKey($event)">
+                <option v-for="reference in keys" v-bind:key="reference">{{ reference }}</option>
+            </select>
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "KeyReference",
+        data: function() {
+            return {
+                selectedKey: String
+            }
+        },
+        methods: {
+          setKey(event) {
+              this.$store.commit('setSelectedKey', event.target.value);
+              this.$store.commit('setSelectedKeyScale');
+          }
+        },
+        computed: {
+            keys() {
+                return this.$store.getters.getKeys;
+            },
+            scales() {
+                return this.$store.getters.getScales;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
