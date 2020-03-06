b91415c948c32b5b3128969dd1e205b19ebb8f35
olivieromassi <oliviero.massi@mail.polimi.it>
2020-03-06 19:55:55 +0100

    Added ChordProgression.vue component

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <KeyReference>Test</KeyReference>
     <Modes>Test</Modes>
+    <ChordProgression>Test</ChordProgression>
   </div>
 </template>
 
 <script>
 import KeyReference from "@/components/KeyReference";
 import Modes from "@/components/Modes";
+import ChordProgression from "@/components/ChordProgression";
 
 export default {
 
   name: 'App',
   components: {
+    ChordProgression,
     Modes,
     KeyReference
   }
---
+++ src/components/ChordProgression.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>CHORD PROGRESSION LIST</h1>
+        <div v-for="chord in modes" v-bind:key="chord">
+                <Chord :keyReference="keyReference" :degree="keyReference.indexOf(chord.tonic)">Test</Chord>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Chord from "@/components/Chord";
+
+    export default {
+        name: "ChordProgression",
+        components: {
+            Chord
+        },
+        computed: {
+            modes() {
+                return this.$store.getters.getModes;
+            },
+            keyReference() {
+                return this.$store.getters.getKeyReference;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
