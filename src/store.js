933bb6ad26d8fb470ee2cd58aba1b088dbe5f38c
olivieromassi <oliviero.massi@mail.polimi.it>
2020-03-07 12:34:55 +0100

    Added some comments to methods and variables in store.js

--- src/store.js
+++ src/store.js
@@ -5,9 +5,16 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
+        /*This list represents the possible key references*/
         keys: ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B'],
+
+        /*This string represents the name of the key reference chosen by the user*/
         selectedKey: String,
+
+        /*This list contains all the notes of the Ionian Scale corresponding to the chosen key reference*/
         keyReference: [],
+
+        /*This dictionary contains all the possible Ionian scales*/
         scales: [['C', 'D', 'E', 'F', 'G', 'A', 'B'],
             ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#'],
             ['D', 'E', 'F#', 'G', 'A', 'B', 'C#'],
@@ -20,7 +27,11 @@ export const store = new Vuex.Store({
             ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
             ['Bb', 'C', 'D', 'Eb', 'F', 'G', 'A'],
             ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#']],
+
+        /*This list contains the 7 major harmony modes related to the selected key reference*/
         modes: [],
+
+        /*This list contains the chords chosen by the user to be part of a chord progression*/
         progression: []
     },
     getters: {
@@ -45,18 +56,27 @@ export const store = new Vuex.Store({
 
     },
     mutations: {
+        /*METHODS RELATED TO THE KEY REFERENCE STATE*/
+        /*This method assigns the selected key to the corresponding state's variable*/
         setSelectedKey(state, key) {
             state.selectedKey = key;
         },
+        /*This method extracts from all the possible scales the one related to the chosen key reference*/
         setSelectedKeyScale(state) {
             state.keyReference = state.scales[state.keys.indexOf(state.selectedKey)];
         },
+
+        /*METHODS RELATED TO THE MODES STATE*/
+        /*This method resets the modes list to an empty list*/
         resetModes(state) {
             state.modes = [];
         },
         addChordToModes(state, features) {
             state.modes.push(features);
         },
+
+        /*METHODS RELATED TO THE PROGRESSION STATE*/
+        /*This method resets the progression list to an empty list*/
         resetProgression(state) {
             state.progression = [];
         },
